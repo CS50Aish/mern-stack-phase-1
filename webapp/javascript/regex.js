505a21df8e429c4e8c91cc4297314f5c321bca37
aishwarya <aishwaryawaghmare15@gmail.com>
2024-02-05 13:37:03 +0000

    Regex Updation

--- webapp/javascript/regex.js
+++ webapp/javascript/regex.js
@@ -8,3 +8,7 @@ console.log("Regex result:",regexResult);
 let dynamicRegexPattern = new RegExp("[0-9]{3}","g");
 let dynamicRegexResult = "123-456-789".match(dynamicRegexPattern);
 console.log(dynamicRegexResult);
+
+let dynamicRegexPattern1 = new RegExp("[0-9]{5}","g");
+let dynamicRegexResult1 = "12345-45678-78A78".match(dynamicRegexPattern1);
+console.log(dynamicRegexResult1);
