d55a090c875ecffd66cafbbe27e8c0a2b918fc61
aishwarya <aishwaryawaghmare15@gmail.com>
2024-02-05 12:33:29 +0000

    Added string file

---
+++ webapp/javascript/strings.js
@@ -0,0 +1,11 @@
+let str1 = "Hello Everyone";
+let str2 = 'Welcome to the session';
+let str3 = `Let learn JS`;
+
+console.log(str1," and type is ", typeof str1);
+console.log(str2, " and type is ", typeof str2);
+console.log(str3, " and type is ", typeof str3);
+
+let str4 = new String("I am a non-primitive object");
+console.log(str4, "and the type is", typeof str4);
+console.log("First character of str1:", str1.charAt(0));
